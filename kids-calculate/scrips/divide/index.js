cef5a9b631021770e00cdee165d929eb9923db08
Sumet Funan <sfunan@celestica.com>
2025-09-22 21:59:47 +0700

    Init divide

---
+++ kids-calculate/scrips/divide/index.js
@@ -0,0 +1,65 @@
+var multiplyObj = {
+    numRows: 1,
+    numRowsHistory: 1,
+}
+
+function validateDivideResult() {
+    let textColor = "green"
+    let symbol = "*"
+
+    let firstNumber = +$('#firstDivideNumber').val();
+    let secondNumber = +$('#secondDivideNumber').val();
+    let resultNumber = +$('#resultDivideNumber').val();
+
+    let isCorrect = validateResult(firstNumber, secondNumber, resultNumber, symbol);
+
+    if(isCorrect.message == "zero") return;
+
+    if (isCorrect.status) {
+        addResultDivideToItemList({ "symbol": symbol, "firstNumber": firstNumber, "secondNumber": secondNumber, "resultNumber": resultNumber })
+        $('#contentDivideResult').show();
+        addSuggestDivideValue();
+    }
+    else {
+        textColor = "red"
+    }
+
+    $('#contentDivideHistory').show();
+    addResultDivideToHistoryList({ "symbol": symbol, "firstNumber": firstNumber, "secondNumber": secondNumber, "resultNumber": resultNumber, "color": textColor })
+}
+
+function addResultDivideToItemList(item) {
+    elementItem = `<div class="row mb-3">
+            <p>${multiplyObj.numRows}) ${item.firstNumber} ${item.symbol} ${item.secondNumber} = ${item.resultNumber}</p>
+                </div>`
+    $('#resultDivideList').append(elementItem)
+
+    clearDivideValue()
+
+    multiplyObj.numRows++;
+}
+
+function addResultDivideToHistoryList(item) {
+    elementItem = `<div class="row mb-3">
+            <p style="color: ${item.color}">${multiplyObj.numRowsHistory}) ${item.firstNumber} ${item.symbol} ${item.secondNumber} = ${item.resultNumber}</p>
+                </div>`
+    $('#historyDivideList').append(elementItem)
+
+    if (item.color == "green") {
+        multiplyObj.numRowsHistory++;
+    }
+}
+
+function clearDivideValue() {
+    $('#firstDivideNumber').val('');
+    $('#secondDivideNumber').val('');
+    $('#resultDivideNumber').val('');
+}
+
+function addSuggestDivideValue() {
+    // $('#firstDivideNumber').val(parseInt((Math.random() * 500) + 500));
+    $('#firstDivideNumber').val(parseInt((Math.random() * 99)));
+    $('#secondDivideNumber').val(parseInt((Math.random() * 9)));
+}
+
+addSuggestDivideValue()
